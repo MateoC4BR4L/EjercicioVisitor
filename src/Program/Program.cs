a746125f2fcab1ed0086eb1408bb26c6d33a87e6
MateoC4BR4L <juan.cabral@correo.ucu.edu.uy>
2022-11-03 17:46:49 -0300

    Parte 1 y 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,24 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Persona p1 = new Persona("Enrique", 75);
+            Persona p2 = new Persona("Jose", 45);
+            Persona p3 = new Persona("Pepe", 45);
+            Persona p4 = new Persona("Juancho", 25);
+            Persona p5 = new Persona("Pablo", 25);
+            Persona p6 = new Persona("El Nacho", 5);
+            Persona p7 = new Persona("El Alexi", 5);
+            Persona p8 = new Persona("El Matuti", 5);
+
+
+            NodePerson n1 = new NodePerson(p1);
+            NodePerson n2 = new NodePerson(p2);
+            NodePerson n3 = new NodePerson(p3);
+            NodePerson n4 = new NodePerson(p4);
+            NodePerson n5 = new NodePerson(p5);
+            NodePerson n6 = new NodePerson(p6);
+            NodePerson n7 = new NodePerson(p7);
+            NodePerson n8 = new NodePerson(p8);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,6 +34,7 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+            n3.AddChildren(n8);
 
             // visitar el árbol aquí
         }
